55c6d0d25b385432c3826b869893f723845706bc
Juan Francisco <juanfrancgalvan@gmail.com>
2025-01-31 19:05:50 -0300

    new file:   README.md 	modified:   build/images/favicon.webp 	deleted:    build/style.css 	deleted:    build/style.css.map 	new file:   build/styles/style.css 	new file:   build/styles/style.css.map 	modified:   gulpfile.js 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	renamed:    src/images/favicon.png -> source/images/favicon.png 	new file:   source/styles/layouts/footer.scss 	new file:   source/styles/layouts/globlals.scss 	new file:   source/styles/layouts/header.scss 	new file:   source/styles/layouts/index.scss 	new file:   source/styles/layouts/main.scss 	new file:   source/styles/layouts/nav.scss 	new file:   source/styles/layouts/responsive.scss 	new file:   source/styles/style.scss 	deleted:    src/style.scss

---
+++ README.md
@@ -0,0 +1,13 @@
+# Creado por Juan Francisco Galván
+
+## Proyecto creado con HTML, CSS, SCSS y Javascript.
+
+### Contacto
+
+https://www.instagram.com/juanfrancgalvan/
+
+https://www.threads.net/@juanfrancgalvan
+
+https://twitter.com/juanfrancgalvan
+
+https://www.linkedin.com/in/juanfrancgalvan/
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,47 @@
-import gulp from 'gulp';
-import imagemin, { mozjpeg, optipng } from 'gulp-imagemin';
-import webp from 'gulp-webp';
+const { src, dest, watch, parallel } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const plumber = require('gulp-plumber')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+const terser = require('gulp-terser-js')
 
-export default () => (
-  gulp.src('src/images/*')
-    .pipe(imagemin([mozjpeg({ quality: 10 }), optipng({ optimizationLevel: 5 })]))
+function css() {
+  return src('source/styles/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/styles'))
+}
+
+function js() {
+  return src('source/scripts/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/scripts'))
+}
+
+function images() {
+  return src('source/images/*')
+    .pipe(imagemin())
     .pipe(webp())
-    .pipe(gulp.dest('build/images'))
-);
+    .pipe(dest('build/images'))
+}
+
+function watchFiles() {
+  watch("source/styles/**/*.scss", css)
+  watch("source/scripts/**/*.js", js)
+}
+
+exports.css = css
+exports.js = js
+exports.images = images
+
+exports.default = parallel(css, js, watchFiles)
